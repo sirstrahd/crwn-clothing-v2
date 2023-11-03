e19bd4b80a2c4a112a083199627422493a35c0ae
Marc Salles <marc.salles@triptease.com>
2023-11-03 16:57:50 +0100

    usercontext stuff

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 import './index.scss';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
+import {UserProvider} from "./contexts/user.context";
 
 const rootElement = document.getElementById('root');
 
 render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  rootElement
+    <React.StrictMode>
+        <BrowserRouter>
+            <UserProvider>
+                <App/>
+            </UserProvider>
+        </BrowserRouter>
+    </React.StrictMode>,
+    rootElement
 );
