6889b69878cad79a5cd5bc366a9ae71c18f0e5ad
Marc Salles <marc.salles@triptease.com>
2024-08-27 10:48:21 +0200

    preview categories
    also load them from firebase

---
+++ src/contexts/categories.context.jsx
@@ -0,0 +1,25 @@
+import {createContext, useEffect, useState} from 'react';
+import SHOP_DATA from '../shop-data.js';
+import {addCollectionAndDocuments, getCategoriesAndDocuments} from "../utils/firebase/firebase.utils";
+
+// as the actual value you want to access
+export const CategoriesContext = createContext({
+    categoriesMap: {},
+})
+
+
+export const CategoriesProvider = ({children}) => {
+    const [categoriesMap, setCategoriesMap] = useState({});
+    useEffect(() => {
+        const getCategoriesMap = async () => {
+            const categoryMap = await getCategoriesAndDocuments();
+            console.log(categoryMap);
+            setCategoriesMap(categoryMap);
+        }
+        getCategoriesMap();
+
+    }, [])
+    const value = {categoriesMap};
+
+    return <CategoriesContext.Provider value={value}>{children}</CategoriesContext.Provider>
+}
